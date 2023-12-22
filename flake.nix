9bd16f101db757edf4bf0178037807535d139308
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2023-12-22 10:55:58 -0300

    Use a Flake template for single shell scripts

--- flake.nix
+++ flake.nix
@@ -1,11 +1,39 @@
 {
-  description = "A very basic flake";
+  description = "Interactive Tmux wrapper for easier session attachment";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages = rec {
+        tmux-attacher = pkgs.stdenv.mkDerivation rec {
+          name = "tmux-attacher";
+          src = ./.;
+          unpackPhase = "true"; # FIXME what does this mean?
+          buildPhase = ":"; # FIXME what does this mean?
+          installPhase =
+            ''
+              mkdir -p "$out/bin"
+              cp "$src/tmux-attacher" "$out/bin/tmux-attacher"
+              chmod +x "$out/bin/tmux-attacher"
+            '';
+        };
+        default = tmux-attacher;
+      };
+
+      apps = rec {
+        tmux-attacher = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.tmux-attacher;
+        };
+        default = tmux-attacher;
+      };
+    }
+  );
 }
